42ed5fdbb5582a13c3d8c8aff0ab72c1a81a6369
Jeanette Huang <jeanette.y.huang@gmail.com>
2013-04-23 18:18:05 -0500

    created stoplist

--- app/controllers/searchedtweets_controller.rb
+++ app/controllers/searchedtweets_controller.rb
@@ -1,3 +1,5 @@
+require 'stoplist'
+
 class SearchedtweetsController < ApplicationController
   def index
     if params[:search] != nil 
@@ -7,17 +9,19 @@ class SearchedtweetsController < ApplicationController
    
   #  @searchedtweets = Tweet.order("created_at desc")
 
-    @hash = Hash.new(0)
-    @text_array = []
-    @searchedtweets.each do |entry|
-      @text_array.push(entry.text.split(' '))
-    end
-    @text_array.each do |tweet|
-      tweet.each do |word|
+  @hash = Hash.new(0)
+  @text_array = []
+  @searchedtweets.each do |entry|
+    @text_array.push(entry.text.split(' '))
+  end
+  @text_array.each do |tweet|
+    tweet.each do |word|
+      unless STOPLIST.include?(word.downcase) or word.downcase == params[:search] or word.downcase == "#" + params[:search]
         @hash[word.downcase] += 1
       end
     end
-    @sortedhash = @hash.sort_by {|_key, value| value}.reverse
+  end
+  @sortedhash = @hash.sort_by {|_key, value| value}.reverse
     
   end
 
