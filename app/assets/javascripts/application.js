286a8da9e18c6f4b0e81e5b3fbe3ad35c8f7577a
Jeanette Huang <jeanette.y.huang@gmail.com>
2013-05-24 19:08:45 -0500

    fixed header so it displays date when doing drilldown and breadcrumbs (added snapshot param to get req)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,13 +28,13 @@ $("a.breadcrumbs").click(function() {
   var oldid = "";
   // $('a.breadcrumbs').first().remove();
   if (stopid == $('.search-input').val()) {
-    $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + stopid + '&stoplistvar=' + $("#stoplist-text").val(), function(response) { $('#main-wrap').html(response);}, 'html');
+    $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + stopid + '&stoplistvar=' + $("#stoplist-text").val() + '&snapshot=' + $('#snapshot-string').text(), function(response) { $('#main-wrap').html(response);}, 'html');
   }
   $("a.breadcrumbs:not(:first)").each(function() {
     var currentid = $(this).attr("id");
     if (currentid == stopid) {
       oldid += ":" + currentid;
-      $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + oldid + '&stoplistvar=' + $("#stoplist-text").val(), function(response) { $('#main-wrap').html(response);}, 'html');
+      $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + oldid + '&stoplistvar=' + $("#stoplist-text").val() + '&snapshot=' + $('#snapshot-string').text(), function(response) { $('#main-wrap').html(response);}, 'html');
     }
     else {
       oldid += ":" + currentid;
--- app/controllers/searchedtweets_controller.rb
+++ app/controllers/searchedtweets_controller.rb
@@ -121,7 +121,7 @@ class SearchedtweetsController < ApplicationController
       chart.legend(enabled: false)
       chart.tooltip(formatter: "function() { s='<b>' + this.series.name + '</b><br/>' + this.x + ': ' + this.y; return s;}")
       chart.plotOptions(bar: {cursor: 'pointer', point: { events: {click: "function() {
-        $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + $('#id-string').text() + ':' + this.category + '&stoplistvar=' + $('#stoplist-string').text(), function(response) { $('#main-wrap').html(response);}, 'html');
+        $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + $('#id-string').text() + ':' + this.category + '&stoplistvar=' + $('#stoplist-string').text() + '&snapshot=' + $('#snapshot-string').text(), function(response) { $('#main-wrap').html(response);}, 'html');
           }".squish}}})
 
     end
