27d1b573a56c51eca36172a9aaa84a131db22322
Allen <allenz456@gmail.com>
2013-06-07 17:15:00 -0500

    added console.logs for when get requests are made

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -13,12 +13,18 @@ function validate() {
 $("a.breadcrumbs").click(function() {
   var stopid = $(this).attr("id");
   var oldid = "";
+  console.log("Processing breadcrumbs....");
+
   if (stopid == $('.search-input').val()) {
       var snapshot = $('#snapshot-string').text();
       if (snapshot == '') {
+        console.log("$.get('/searchedtweets/_makedata?search='" + $('.search-input').val() + "'&id='" + oldid + "'&stoplistvar='" + $("#stoplist-text").val() +", function(response) { $('#main-wrap').html(response);}, 'html');");        
+
         $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + oldid + '&stoplistvar=' + $("#stoplist-text").val(), function(response) { $('#main-wrap').html(response);}, 'html');
       }
       else {
+        console.log("$.get('/searchedtweets/_makedata?search='" + $('.search-input').val() + "'&id='" + oldid + "'&stoplistvar='" + $("#stoplist-text").val() + "'&snapshot='" + snapshot +", function(response) { $('#main-wrap').html(response);}, 'html');");
+
         $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + oldid + '&stoplistvar=' + $("#stoplist-text").val() + '&snapshot=' + snapshot, function(response) { $('#main-wrap').html(response);}, 'html');
       }
   }
@@ -29,9 +35,13 @@ $("a.breadcrumbs").click(function() {
 
       var snapshot = $('#snapshot-string').text();
       if (snapshot == '') {
+        console.log("$.get('/searchedtweets/_makedata?search='" + $('.search-input').val() + "'&id='" + oldid + "'&stoplistvar='" + $("#stoplist-text").val() +", function(response) { $('#main-wrap').html(response);}, 'html');");
+
         $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + oldid + '&stoplistvar=' + $("#stoplist-text").val(), function(response) { $('#main-wrap').html(response);}, 'html');
       }
       else {
+        console.log("$.get('/searchedtweets/_makedata?search='" + $('.search-input').val() + "'&id='" + oldid + "'&stoplistvar='" + $("#stoplist-text").val() + "'&snapshot='" + snapshot +", function(response) { $('#main-wrap').html(response);}, 'html');");
+
         $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + oldid + '&stoplistvar=' + $("#stoplist-text").val() + '&snapshot=' + snapshot, function(response) { $('#main-wrap').html(response);}, 'html');
       }
     }
@@ -46,23 +56,39 @@ $('a.dropdown-toggle').click(function(event) {
   $('ul#menu1').toggle();  
 })
 
-// Stoplist
+
 $(document).ready(function() {
   $('.modal-backdrop').remove();
+
+  // Snapshots
   $("a.inserted-at").click(function() {
     var stoplistvar = "";
+    console.log("Processing Snapshot....");
+    console.log("executing get request");
+    console.log("$.get('/searchedtweets/_makedata?search='" + $('.search-input').val() + "'&id='" + $('.search-input').val() + "'&stoplistvar='" + stoplistvar + "'&snapshot='" + $(this).attr("id") + ", function(response) { $('#main-wrap').html(response);}, 'html');");
+
     $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + $('.search-input').val() + '&stoplistvar=' + stoplistvar + '&snapshot=' + $(this).attr("id"), function(response) { $('#main-wrap').html(response);}, 'html');
   });
+
+// Stoplist  
   $("#stoplist-submit").click(function() {
     var oldid = "";
+    console.log("Processing Stoplist....");
+    
     $("a.breadcrumbs:not(:first)").each(function() {
       var currentid = $(this).attr("id");
       oldid += ":" + currentid;
     });
     var snapshot = $('#snapshot-string').text();
     if (snapshot == '') {
+      console.log("executing get request");
+      console.log("$.get('/searchedtweets/_makedata?search='" + $('.search-input').val() + "'&id='" + oldid + "'&stoplistvar='" + $("#stoplist-text").val() + ", function(response) { $('#main-wrap').html(response);}, 'html');");
+
       $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + oldid + '&stoplistvar=' + $("#stoplist-text").val(), function(response) { $('#main-wrap').html(response);}, 'html');
     } else {
+      console.log("executing get request");
+      console.log("$.get('/searchedtweets/_makedata?search='" + $('.search-input').val() + "'&id='" + oldid + "'&stoplistvar='" + $("#stoplist-text").val() + "'&snapshot='" + snapshot + ", function(response) { $('#main-wrap').html(response);}, 'html');");
+
       $.get('/searchedtweets/_makedata?search=' + $('.search-input').val() + '&id=' + oldid + '&stoplistvar=' + $("#stoplist-text").val() + '&snapshot=' + snapshot, function(response) { $('#main-wrap').html(response);}, 'html');
     }
   })
